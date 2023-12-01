601ae681335fb1c77b364d65570bc8da353a487d
Abdilmajid Ibrahim <abdilwebdev@gmail.com>
2023-12-01 12:27:09 -0500

    provisiond control and installed ansible

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,15 @@
 % export AWS_ACCESS_KEY_ID="anaccesskey"
 % export AWS_SECRET_ACCESS_KEY="asecretkey"
 % export AWS_REGION="us-east-1"
+- we can also store credentials inside files
+Ex: 
+"~/.aws/credentials"
+[default]
+aws_access_key_id="accesskey"
+aws_secret_access_key="secretkey"
+"~/.aws/config"
+[default]
+region=us-east-1
 */
 
 # This provisions the control node 
@@ -12,7 +21,15 @@ resource "aws_instance" "control" {
   instance_type = var.instance_control
   # if no default subnet, then we can use setup_id
   # make sure to use correct subnet
-  subnet_id = "subnet-064789520be471d06"
+  subnet_id = "subnet-037d9537bf26aa812"
+
+  # below will update the repo, then install epel repo which contains our ansible package, then we install the anisible package
+  user_data = <<EOF
+              sudo dnf update -y
+              sudo dnf install \
+              https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
+              sudo dnf install ansible -y
+              EOF
 
   tags = {
     Name = "control"
@@ -20,14 +37,24 @@ resource "aws_instance" "control" {
   }
 }
 
-# This provisions managed node1
-resource "aws_instance" "node1" {
-  ami = var.ami
-  instance_type = var.instance_managed
+# # This block provisions managed node1
+# resource "aws_instance" "node1" {
+#   ami = var.ami
+#   instance_type = var.instance_managed
 
-  tags = {
-    Name = "node1"
-    Description = "Ansbile Managed Node"
-  }
-}
+#   tags = {
+#     Name = "node1"
+#     Description = "Ansbile Managed Node"
+#   }
+# }
 
+# # This block provisions managed node2
+# resource "aws_instance" "node1" {
+#   ami = var.ami
+#   instance_type = var.instance_managed
+
+#   tags = {
+#     Name = "node2"
+#     Description = "Ansbile Managed Node2"
+#   }
+# }
