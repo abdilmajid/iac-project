260fec6296335fe051469d94bb869eabfc18e850
Abdilmajid Ibrahim <abdilwebdev@gmail.com>
2023-12-07 12:03:30 -0500

    changed packer folder name to images, created ssh keys

---
+++ images/base-centos9-ami/scripts/ansible_sudo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# allow sudo access without entering password
+echo "ansible  ALL=(ALL) NOPASSWD:ALL" 
--- packer/base-centos9-ami/scripts/ansible_sudo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# allow sudo access without entering password
-echo "ansible  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible
