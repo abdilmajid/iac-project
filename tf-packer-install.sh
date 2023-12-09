8534b88b36eb31913e498a8bd02c263959488936
Abdilmajid Ibrahim <abdilwebdev@gmail.com>
2023-12-09 09:54:27 -0500

    updated tf-packer-install.sh

--- tf-packer-install.sh
+++ tf-packer-install.sh
@@ -3,9 +3,22 @@
 # This checks OS, then assings to variable "OS"
 OS=$(grep -i ^name /etc/os-release |cut -f2 -d"="|sed 's/"//g')
 
+# check if packer and terraform already installed
+CHECK=$(packer version && terraform version)
+# if already installed then stdout would be 0, if not then 1
+CHECK=$(echo $?)
+
 # -- Below terraform and packer packages installed based on current OS
+# Check if packages already installed
+if [ "$CHECK" -eq 0 ]; then
+	echo "Terreform and Packer already installed"
+# installs terraform and packer on Debian/Ubuntu
+elif [ "$OS" = "Ubuntu" ]; then
+	wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+	sudo apt update && sudo apt install terraform packer
 # installs terraform and packer on CentOS/RHEL
-if [[ $OS = "Red Hat Enterprise Linux" ]]; then
+elif [[ $OS = "Red Hat Enterprise Linux" ]]; then
 	sudo yum install -y yum-utils
 	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 	sudo yum -y install terraform packer
@@ -14,19 +27,20 @@ elif [[ $OS = "Amazon Linux" ]]; then
 	sudo yum install -y yum-utils shadow-utils
 	sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 	sudo yum -y install terraform packer
-# installs terraform and packer on Debian/Ubuntu
-elif [[ $OS = "Ubuntu" ]]; then
-	wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-	echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-	sudo apt update && sudo apt install terraform packer
 else
 	echo "Sorry, cant run this script"
 fi
 
 # -- Check that everything installed correctly
 # Packer version
+echo 
+echo " ==== PACKER VERSION ==== "
 packer version
-
+echo  "-------------------------"
+echo 
 # Terraform Version
+echo " ==== TERRAFORM VERSION ==== "
 terraform version
+echo  "-------------------------"
+echo 
 
